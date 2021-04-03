5d1822bce6ba93f1c7f25bbd1bf76d4e511b66b6
Fernand LIME <fernand.lime@gmail.com>
2021-04-04 01:02:13 +0200

    Feat: implement getCityByName call with useQuery

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { SafeAreaProvider } from "react-native-safe-area-context"
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
@@ -17,45 +12,6 @@ const client = new ApolloClient({
   uri: "https://graphql-weather-api.herokuapp.com/",
   cache: new InMemoryCache(),
 })
-
-client
-  .query({
-    query: gql`
-      getCityByName(name: "Gothenburg") {
-        id
-        name
-        country
-        coord {
-          lon
-          lat
-        }
-        weather {
-          summary {
-            title
-            description
-            icon
-          }
-          temperature {
-            actual
-            feelsLike
-            min
-            max
-          }
-          wind {
-            speed
-            deg
-          }
-          clouds {
-            all
-            visibility
-            humidity
-          }
-          timestamp
-        }
-      }
-    `
-  })
-  .then((result) => console.log(result))
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,61 @@
 import * as React from "react"
 import { StyleSheet } from "react-native"
+import { gql, useQuery } from "@apollo/client"
 
 import EditScreenInfo from "../components/EditScreenInfo"
 import { Text, View } from "../components/Themed"
 
+const GET_CITY_BY_NAME = gql`
+  query GetCityByName($name: String!) {
+    getCityByName(name: $name) {
+      id
+      name
+      country
+      coord {
+        lon
+        lat
+      }
+      weather {
+        summary {
+          title
+          description
+          icon
+        }
+        temperature {
+          actual
+          feelsLike
+          min
+          max
+        }
+        wind {
+          speed
+          deg
+        }
+        clouds {
+          all
+          visibility
+          humidity
+        }
+        timestamp
+      }
+    }
+  }
+`
+
 export default function TabOneScreen() {
+  const { loading, error, data } = useQuery(GET_CITY_BY_NAME, {
+    variables: { name: "Gothenburg" },
+  })
+  if (loading) {
+    return <Text>Loading ...</Text>
+  }
+
+  if (error) {
+    console.log({ error })
+  }
+
+  console.log({ data })
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
