9290b3f558ad648828bea756cb4b7cab8177556a
Fernand LIME <fernand.lime@gmail.com>
2021-04-03 21:31:22 +0200

    Feat: implement getCityByName call

--- App.tsx
+++ App.tsx
@@ -1,10 +1,61 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
 import Navigation from "./navigation"
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: "https://graphql-weather-api.herokuapp.com/",
+  cache: new InMemoryCache(),
+})
+
+client
+  .query({
+    query: gql`
+      getCityByName(name: "Gothenburg") {
+        id
+        name
+        country
+        coord {
+          lon
+          lat
+        }
+        weather {
+          summary {
+            title
+            description
+            icon
+          }
+          temperature {
+            actual
+            feelsLike
+            min
+            max
+          }
+          wind {
+            speed
+            deg
+          }
+          clouds {
+            all
+            visibility
+            humidity
+          }
+          timestamp
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result))
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
@@ -14,10 +65,12 @@ export default function App() {
     return null
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <ApolloProvider client={client}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </ApolloProvider>
     )
   }
 }
