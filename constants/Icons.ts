c2b31cf119850998733a33d68984680484844207
Fernand LIME <fernand.lime@gmail.com>
2021-04-04 09:26:36 +0200

    Assets: add weather icons

---
+++ constants/Icons.ts
@@ -0,0 +1,39 @@
+const lottie01d = require("../assets/lottie/01d.json")
+const lottie01n = require("../assets/lottie/01n.json")
+const lottie02d = require("../assets/lottie/02d.json")
+const lottie02n = require("../assets/lottie/02n.json")
+const lottie03d = require("../assets/lottie/03d.json")
+const lottie03n = require("../assets/lottie/03n.json")
+const lottie04d = require("../assets/lottie/04d.json")
+const lottie04n = require("../assets/lottie/04n.json")
+const lottie09d = require("../assets/lottie/09d.json")
+const lottie09n = require("../assets/lottie/09n.json")
+const lottie10d = require("../assets/lottie/10d.json")
+const lottie10n = require("../assets/lottie/10n.json")
+const lottie11d = require("../assets/lottie/11d.json")
+const lottie11n = require("../assets/lottie/11n.json")
+const lottie13d = require("../assets/lottie/13d.json")
+const lottie13n = require("../assets/lottie/13n.json")
+const lottie50d = require("../assets/lottie/50d.json")
+const lottie50n = require("../assets/lottie/50n.json")
+
+export default {
+  "01d": lottie01d,
+  "01n": lottie01n,
+  "02d": lottie02d,
+  "02n": lottie02n,
+  "03d": lottie03d,
+  "03n": lottie03n,
+  "04d": lottie04d,
+  "04n": lottie04n,
+  "09d": lottie09d,
+  "09n": lottie09n,
+  "10d": lottie10d,
+  "10n": lottie10n,
+  "11d": lottie11d,
+  "11n": lottie11n,
+  "13d": lottie13d,
+  "13n": lottie13n,
+  "50d": lottie50d,
+  "50n": lottie50n,
+}
